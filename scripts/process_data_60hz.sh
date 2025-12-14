140b46be4711a3973db21020a601aa033df9f224
MrSecant <zhengyh021@gmail.com>
2025-12-14 12:23:25 +0800

    add process_data_all_zarr_60hz

---
+++ scripts/process_data_60hz.sh
@@ -0,0 +1,37 @@
+# python tools/process_data_all.py \
+#     --root_path /home/tars/projects/raw_data \
+#     --save_path /home/tars/projects/dataset \
+#     --task_list vase_sponge_test \
+    # --save_camera_vis &
+
+python tools/process_data_all_zarr_60hz.py \
+    --root_path /home/tars/projects/raw_data \
+    --save_path /home/tars/projects/dataset_60z \
+    --task_list vase_sponge_test1 \
+    # --episode_length 2
+    # --save_camera_vis 
+
+# python tools/process_data_all.py \
+#     --root_path /home/tars/projects/raw_data \
+#     --save_path /home/tars/projects/dataset \
+#     --task_list vase_sponge_test vase_sponge_test1 
+
+
+# python tools/process_data_all.py \
+#     --root_path /mnt/sda/tactile_dataset/vase/ \
+#     --save_path /home/tars/projects/dataset \
+#     --task_list vase2_new_A vase2_new_B vase2_new_C 
+    # --save_camera_vis
+
+
+# python tools/process_data_all.py \
+#     --root_path /mnt/sda/tactile_dataset/vase/ \
+#     --save_path /home/tars/projects/dataset \
+#     --task_list vase2_new_B &
+#     # --save_camera_vis
+
+# python tools/process_data_all.py \
+#     --root_path /mnt/sda/tactile_dataset/vase/ \
+#     --save_path /home/tars/projects/dataset \
+#     --task_list vase2_new_C \
+    # --save_camera_vis
